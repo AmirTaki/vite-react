c08194eba0752ac3c60b026df9ae552dd0774327
amir <amirtaki623@gmail.com>
2025-06-01 03:23:57 +0330

    Input Field onChange Event

---
+++ src/componets/LoginForm.jsx
@@ -0,0 +1,47 @@
+export function LoginForm() {
+    return(
+        <form
+            // method = "POST" action="/api/login"
+            onSubmit={
+                (e) => {
+                    e.preventDefault()
+                    const formData = new FormData(e.target)
+                    const username = formData.get("username")
+                    const password = formData.get('password')
+                    fetch("http://localhost:5173/api/login",{
+                        body : {
+                            username,
+                            password,
+                        },
+                        method : "POST",
+                    })
+                }   
+            }     
+        >
+            <label htmlFor="username">Username</label>
+            <br/>
+            <input 
+                type="text"
+                id = "username" 
+                onChange={(e)=>{
+                    console.log(`username : ${e.target.value}`);
+                }}
+                name = "username"
+            />
+            <br/>
+            
+            <label htmlFor="password">Password</label>
+            <br/>
+            <input 
+                type="password" 
+                id = "password" 
+                onChange={(e)=>{
+                    console.log(`password : ${e.target.value}`);
+                }}
+                name = "password"
+            />
+            <br/>
+            <button>Login</button>
+        </form>
+    )
+}
--- src/componets/UserDetails.jsx
+++ src/componets/UserDetails.jsx
@@ -1,49 +1,5 @@
-import PropTypes from "prop-types"
 
-
-export function UserDetails ({user}){  
-  
-    return(
-       <div>
-            <b>ID : </b>  <span>{user.id}</span>
-            <br/>
-            <b>Username : </b> <span>{user.username}</span>
-            <br/>
-            <b>email :</b> <span>{user.eamil}</span> 
-        </div>
-    )
+export function UserDetails (){    
+    
 }
 
-UserDetails.propTypes = {
-    user : PropTypes.shape({
-        id : PropTypes.number.isRequired,
-        username : PropTypes.string.isRequired,
-        eamil : PropTypes.string.isRequired
-    })
-}
-
-
-
-// export function UserDetails (props){
-  
-    //   console.log(
-    //                <div>
-    //             <b>ID : </b>  <span>{props.user.id}</span>
-    //             <br/>
-    //             <b>Username : </b> <span>{props.user.username}</span>
-    //             <br/>
-    //             <b>email :</b> <span>{props.user.eamil}</span> 
-    //         </div>
-
-//     )
-//     console.log(props)
-//     return(
-//        <div>
-//             <b>ID : </b>  <span>{props.user.id}</span>
-//             <br/>
-//             <b>Username : </b> <span>{props.user.username}</span>
-//             <br/>
-//             <b>email :</b> <span>{props.user.eamil}</span> 
-//         </div>
-//     )
-// }
