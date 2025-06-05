d91c28e58de5f6b3ec2f476c5dde0c76119d0160
amir <amirtaki623@gmail.com>
2025-06-05 18:56:04 +0330

    useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,24 @@
-import { useState } from "react"
-import { UserDetails } from "./componets/UserDetails"
+import { useState, useEffect } from "react"
+
 
 export default function App(){
-  const [username, setUserName] = useState();
-  const [email, setEmail] = useState();
-  const [counter, setCounter] = useState(3);
+  const [counter, setCounter] = useState(0);
 
-  const [users, setUsers] = useState([
-    {
-      id : 1, 
-      username : "amir",
-      email : "amir@gmail.com",
-    },
-    {
-      id : 2, 
-      username : "mani",
-      email : "mani@gmail.com",
-    }
-  ])
+  // useEffect(()=>{} {})
+  useEffect(()=>{
+    console.log('renderning >>>>>> ')
+    document.title = "React Tutorial" + counter;
+  })
+ 
   return(
     <>
-      <form
-        action=""
-        onSubmit={(e)=>{
-          e.preventDefault();
-          const newUser = {
-            id : counter,
-            username, 
-            email
-          }
-          setCounter((currentCounter) => currentCounter + 1);
-          setUsers ((currentUsersState) => [...currentUsersState, newUser]);
-        }}
+      <div>You Clicked the button {counter} times</div>
+      <button
+        onClick={()=>setCounter((count) =>count + 1)}
       >
-        <div>
-          <label htmlFor="username">Username</label>
-          <input 
-            name = "username" 
-            id = "username" 
-            value = {username}
-            onChange={(e)=>{
-              setUserName(e.target.value);
-            }}  
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input  
-            name = "email" 
-            id = "email" 
-            value = {email}
-            onChange={(e)=>{
-              setEmail(e.target.value);
-            }}
-            />   
-        </div>     
-        <button>Add User</button>
-      </form>
-      <br/>
-      {users.map((user, index)=>(
-        <UserDetails key = {index} user = {user} setUsers = {setUsers}/>
-      ))}
+        Click Me</button>
     </>
   )
+  
 }
 
