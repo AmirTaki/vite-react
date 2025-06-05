1642eaaf3beb709efc9493aabd34d82d31d3096e
amir <amirtaki623@gmail.com>
2025-06-05 04:13:23 +0330

    Binding State to Input Fields part 1

--- src/componets/RegisterForm.jsx
+++ src/componets/RegisterForm.jsx
@@ -1,31 +1,53 @@
 import { useState } from "react"
 
-
 export function RegisterForm(){
-
-    console.log(useState("hello_world"));
-
-    const result = useState();
-    console.log(result[0]);
-    console.log(result[1]);
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [displayName, setDisplayName] = useState("");
+    const isDisabld = !username || !password || !displayName;
     return(
         <form>
             <div>
                 <label htmlFor="username">Username</label>
-                <input type="text" id = "username" />
+                <input type="text" 
+                    id = "username"
+                    value = {username} 
+                    onChange={(e)=>{
+                        setUsername(e.target.value);
+                    }}
+                />
             </div>
             <div>
                 <label htmlFor="password">Passoword</label>
-                <input type="password" id = "password" />
+                <input type="password" 
+                    id = "password" 
+                    value = {password}
+                    onChange={(e)=>{
+                        setPassword(e.target.value);
+                    }}
+                />
             </div>
             <div>
                 <label htmlFor="displayName">Display Name</label>
-                <input type="text" id = "displayName" />
+                <input type="text" 
+                    id = "displayName" 
+                    value = {displayName} 
+                    onChange={(e)=>{
+                        setDisplayName(e.target.value);
+                    }}
+                />
             </div>
+            <div>
+                <span>Username : {username}</span>
+            </div>
+            <div>
+                <span>Password : {password}</span>
+            </div>
+            <div>
+                <span>Display Name : {displayName}</span>
+            </div>
+            <button disabled={isDisabld}>Sign Up</button>
         </form>
     )
 }
